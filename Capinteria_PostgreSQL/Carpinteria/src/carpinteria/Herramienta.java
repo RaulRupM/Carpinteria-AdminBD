e97af0a16ff0a8da8a0eac8a2d4f53799f13ecdd
Raul <a263775@alumnos.uaslp.mx>
2023-05-04 00:08:07 -0600

    Tabla herramienta

--- Capinteria_PostgreSQL/Carpinteria/src/carpinteria/Herramienta.java
+++ Capinteria_PostgreSQL/Carpinteria/src/carpinteria/Herramienta.java
@@ -4,10 +4,10 @@
  */
 package carpinteria;
 
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.Statement;
 
 import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
@@ -16,7 +16,7 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author raul
  */
-public class Insumo extends javax.swing.JFrame {
+public class Herramienta extends javax.swing.JFrame {
 
     private String HOST = "localhost";
     private String PUERTO = "5432";
@@ -28,11 +28,11 @@ public class Insumo extends javax.swing.JFrame {
     public String id;
     public String fecha;
 
-    public Insumo() {
+    public Herramienta() {
         initComponents();
         asociarEventosBotones();
         asociarEventosTabla();
-        llenarTablaInsumo();
+        llenarTablaHerramienta();
     }
 
     public Connection getConexion() {
@@ -41,49 +41,65 @@ public class Insumo extends javax.swing.JFrame {
             Class.forName("org.postgresql.Driver");
             con = DriverManager.getConnection(url, USER, PASS);
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error en la conexion" + e.getMessage(), "Error",
-                    JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error en la conexion" + e);
         }
         return con;
+    }
+
+    private void llenarTablaHerramienta(){
+        try {
+            conexion = getConexion();
+            Statement st = conexion.createStatement();
+            String sql = "SELECT * FROM Proyecto.Herramienta";
+            String [] datos = new String [5];
+            ResultSet rs = st.executeQuery(sql);
+            DefaultTableModel modelo = new DefaultTableModel();
+            modelo.addColumn("idHerramienta");
+            modelo.addColumn("nombre");
+            modelo.addColumn("tipo");
+            modelo.addColumn("estado");
+            modelo.addColumn("cantidadDisponible");
+            jTable1.setModel(modelo);
+            while (rs.next()) {
+                datos[0] = rs.getString("idHerramienta");
+                datos[1] = rs.getString("nombre");
+                datos[2] = rs.getString("tipo");
+                datos[3] = rs.getString("estado");
+                datos[4] = rs.getString("cantidad_disponible");
+                modelo.addRow(datos);
+            }
+            jTextField1.setText("");
+            jTextField2.setText("");
+            jTextField3.setText("");
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al llenar la tabla" + e);
+        }
     }
 
     private void asociarEventosBotones(){
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-
-            private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
-                eliminarInsumo();
+                eliminarHerramienta();
             }
         });
+
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
-            }
-
-            private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {
-                modificarInsumo();
+                modificarHerramienta();
             }
         });
+
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
-            }
-
-            private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {
-                insertarInsumo();
+                insertarHerramienta();
             }
         });
+
     }
 
     private void asociarEventosTabla(){
         jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jTable1MouseClicked(evt);
-            }
-
-            private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {
                 int fila = jTable1.getSelectedRow();
                 id = jTable1.getValueAt(fila, 0).toString();
                 jTextField1.setText(jTable1.getValueAt(fila, 1).toString());
@@ -93,70 +109,39 @@ public class Insumo extends javax.swing.JFrame {
         });
     }
 
-    private void llenarTablaInsumo() {
+    private void insertarHerramienta(){
         try {
             conexion = getConexion();
             Statement st = conexion.createStatement();
-            String sql = "SELECT * FROM Proyecto.Insumo";
-            String[] datos = new String[4];
-            ResultSet rs = st.executeQuery(sql);
-            DefaultTableModel modelo = new DefaultTableModel();
-            modelo.addColumn("idInsumo");
-            modelo.addColumn("nombre");
-            modelo.addColumn("cantidad");
-            modelo.addColumn("precio");
-            jTable1.setModel(modelo);
-            while (rs.next()) {
-                datos[0] = rs.getString("idInsumo");
-                datos[1] = rs.getString("nombre");
-                datos[2] = rs.getString("cantidad");
-                datos[3] = rs.getString("precio");
-                modelo.addRow(datos);
-            }
-            jTextField1.setText("");
-            jTextField2.setText("");
-            jTextField3.setText("");
+            String sql = "INSERT INTO Proyecto.Herramienta (nombre, tipo, estado, cantidad_disponible) VALUES ('" + jTextField1.getText() + "', '" + jTextField2.getText() + "', '" + jTextField3.getText() + "', '" + 0 + "')";
+            st.executeUpdate(sql);
+            llenarTablaHerramienta();
         } catch (Exception e) {
-            // TODO: handle exception
+            JOptionPane.showMessageDialog(null, "Error al insertar la herramienta" + e);
         }
     }
 
-    private void insertarInsumo(){
+    private void modificarHerramienta(){
         try {
             conexion = getConexion();
             Statement st = conexion.createStatement();
-            String sql = "INSERT INTO Proyecto.Insumo (nombre, cantidad, precio) VALUES ('"+jTextField1.getText()+"', '"+jTextField2.getText()+"', '"+jTextField3.getText()+"')";
+            String sql = "UPDATE Proyecto.Herramienta SET nombre = '" + jTextField1.getText() + "', tipo = '" + jTextField2.getText() + "', estado = '" + jTextField3.getText() + "' WHERE idHerramienta = " + id;
             st.executeUpdate(sql);
-            llenarTablaInsumo();
+            llenarTablaHerramienta();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al insertar insumo" + e.getMessage(), "Error",
-                    JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error al modificar la herramienta" + e);
         }
     }
 
-    private void modificarInsumo(){
+    private void eliminarHerramienta(){
         try {
             conexion = getConexion();
             Statement st = conexion.createStatement();
-            String sql = "UPDATE Proyecto.Insumo SET nombre = '"+jTextField1.getText()+"', cantidad = '"+jTextField2.getText()+"', precio = '"+jTextField3.getText()+"' WHERE idInsumo = '"+id+"'";
+            String sql = "DELETE FROM Proyecto.Herramienta WHERE idHerramienta = " + id;
             st.executeUpdate(sql);
-            llenarTablaInsumo();
+            llenarTablaHerramienta();
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al modificar insumo" + e.getMessage(), "Error",
-                    JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void eliminarInsumo(){
-        try {
-            conexion = getConexion();
-            Statement st = conexion.createStatement();
-            String sql = "DELETE FROM Proyecto.Insumo WHERE idInsumo = '"+id+"'";
-            st.executeUpdate(sql);
-            llenarTablaInsumo();
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Error al eliminar insumo" + e.getMessage(), "Error",
-                    JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Error al eliminar la herramienta" + e);
         }
     }
     /**
@@ -165,10 +150,11 @@ public class Insumo extends javax.swing.JFrame {
      * regenerated by the Form Editor.
      */
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
@@ -178,20 +164,25 @@ public class Insumo extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton2.setText("Modificar");
+        jButton2.setName("btnModificar"); // NOI18N
+
+        jButton3.setText("Insertar");
+        jButton3.setName("btnInsertar"); // NOI18N
 
         jLabel1.setText("Nombre");
 
         jTextField1.setName("txtBoxNombre"); // NOI18N
 
-        jLabel2.setText("Cantidad");
+        jLabel2.setText("Tipo");
 
         jTextField2.setName("txtBoxCantidad"); // NOI18N
 
-        jLabel3.setText("Precio");
+        jLabel3.setText("Estado");
 
         jTextField3.setName("textBoxPrecio"); // NOI18N
 
@@ -216,39 +207,36 @@ public class Insumo extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Modificar");
-        jButton2.setName("btnModificar"); // NOI18N
-
-        jButton3.setText("Insertar");
-        jButton3.setName("btnInsertar"); // NOI18N
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addContainerGap()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
+                                        .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
-                                .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 64, Short.MAX_VALUE)
-                                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 91, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 85, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 579, Short.MAX_VALUE))
+                                .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE)))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -269,7 +257,9 @@ public class Insumo extends javax.swing.JFrame {
                     .addComponent(jButton1)
                     .addComponent(jButton2)
                     .addComponent(jButton3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 35, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(35, Short.MAX_VALUE))
         );
@@ -277,22 +267,18 @@ public class Insumo extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }// GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
-        // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code
-        // (optional) ">
-        /*
-         * If Nimbus (introduced in Java SE 6) is not available, stay with the default
-         * look and feel.
-         * For details see
-         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -302,20 +288,20 @@ public class Insumo extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Insumo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Herramienta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Insumo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Herramienta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Insumo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Herramienta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Insumo.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Herramienta.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        // </editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Insumo().setVisible(true);
+                new Herramienta().setVisible(true);
             }
         });
     }
@@ -327,6 +313,7 @@ public class Insumo extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
